b9c83fe8dc1ff1f810841cd2d574bc02e7f304df
durchhaltenbitte <shuang.ind@gmail.com>
2019-09-12 09:31:11 +0200

    add counting function

--- guessr.py
+++ guessr.py
@@ -1,11 +1,13 @@
 import random
 r = random.randint(1,100)
+times = 1
 
 while 1 :
+    times = times + 1
     g = input('猜一个1到100的数字:')
     g = int(g)
     if r == g:
-        print('猜对啦，快去领取奖励吧！')
+        print('哇哦!', times, '次就猜对了呢，快去领取奖励吧！')
         break
     elif r < g:
         print('比这个数小噢')
